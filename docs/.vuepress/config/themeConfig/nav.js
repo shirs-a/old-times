a6a41cb14bf89b5df4f7ee7e35266cc50bfc4fa5
shirs <599073585@qq.com>
2021-06-15 23:58:17 +0800

    单例模式&@PostConstruce注解

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -26,7 +26,9 @@ module.exports = [
     items: [
       { text: '新建spring boot项目', link: '/pages/8309a5b876fc95e3/' },
       { text: '集合', link: '/pages/8309a5b876fc95e5/' },
-      { text: 'JVM', link: '/pages/8309a5b876fc95e4/' }
+      { text: 'JVM', link: '/pages/8309a5b876fc95e4/' },
+      { text: '设计模式', link: '/pages/8309a5b876fc9001/' },
+      { text: '@PostConstruct', link: '/pages/8309a5b876fc9002/' }
     ],
   },
   {
